6dac9721c2f7d5a9d9c4cdf088b4839cf2574051
PavlovStanislav <73657304+PavlovStanislav@users.noreply.github.com>
2021-04-17 18:40:29 +0300

    Add files via upload

---
+++ MVC/WebMVCR2/Controllers/MyController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVCR2.Models;
+using System.Web.Routing;
+
+namespace WebMVCR2.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: Home
+
+        private static PersonRepository db = new PersonRepository();
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            ViewData["Mes"] = "хорошего настроения";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+        }
+
+        public ViewResult OutputData()
+        {
+            ViewBag.Pers = db.GetAllResponses;
+            ViewBag.Count = db.NumberOfPerson;
+            return View("ListPerson");
+        }
+
+        //public string Index(string hel)
+        //{
+        //    string Greeting = ModelClass.ModelHello() + ", " + hel;
+        //    return Greeting;
+        //}
+    }
+
+}
