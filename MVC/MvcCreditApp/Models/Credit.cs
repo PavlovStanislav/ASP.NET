d1d79f60104641b22984c24827d2d1ffb42ab3f2
PavlovStanislav <73657304+PavlovStanislav@users.noreply.github.com>
2021-04-22 10:40:01 +0300

    Add files via upload

---
+++ MVC/MvcCreditApp/Models/Credit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcCreditApp1.Models
+{
+    public class Credit
+    {
+        public virtual int CreditId { get; set; }
+        [DisplayName("Имя Название")]
+        [Required]
+        public virtual string Head { get; set; }
+        [DisplayName("Период")]
+        [Required]
+        public virtual int Period { get; set; }
+        [DisplayName("Сумма кредита")]
+        [Required]
+        public virtual int Sum { get; set; }
+        [DisplayName("Процентная ставка")]
+        [Required]
+        public virtual int Procent { get; set; }
+    }
+}
